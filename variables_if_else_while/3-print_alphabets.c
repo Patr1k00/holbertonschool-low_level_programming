aca98fd7ba183addc4d5f6a57deb7ab51ccc61e3
Patr1k00 <shalsipatrik00@gmail.com>
2023-02-20 04:37:48 -0800

    alo

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* This is header number 4 */
+
+/**
+ * main - Prints the last digit of a randomly generated number
+ * and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
