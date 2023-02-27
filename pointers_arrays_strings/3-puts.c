587a52e5e3001c799acc3d0f0893cc440b210512
Patr1k00 <shalsipatrik00@gmail.com>
2023-02-27 04:47:15 -0800

    alo

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _puts - Prints a string
+  * @str: The string to print
+  *
+  * Return: void
+  */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\n')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
