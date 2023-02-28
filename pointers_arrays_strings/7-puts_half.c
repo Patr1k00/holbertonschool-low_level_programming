bff67aab7a1d492c496cb5e43e3acaf35a976707
Patr1k00 <shalsipatrik00@gmail.com>
2023-02-28 04:15:26 -0800

    alo

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: The string to print
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int k;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 1)
+	{
+		k = (i - 1) / 2;
+		k += 1;
+	}
+	else
+	{
+		k = i / 2;
+	}
+
+	for (; k < i; k++)
+	{
+		_putchar(str[k]);
+	}
+
+	_putchar('\n');
+
+}
+
