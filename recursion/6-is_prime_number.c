34ba9db83e966e7f4ca59af5fa2547c044a53c41
Patr1k00 <shalsipatrik00@gmail.com>
2023-03-16 03:28:15 -0700

    alo

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * find_multipliers - look for multipliers of n.
  * @n: base number.
  * @i: iterator number.
  * Return: 1 if n is prime, 0 otherwise.
  */
+
 int find_multipliers(int n, int i)
 {
 	if (i == n)
@@ -14,12 +16,14 @@ int find_multipliers(int n, int i)
 	else
 		return (find_multipliers(n, i + 1));
 }
+
 /**
  * is_prime_number - check if n is prime
  * @n: base number.
  *
  * Return: 1 if n is prime, 0 otherwise.
  */
+
 int is_prime_number(int n)
 {
 	if (n <= 1)
