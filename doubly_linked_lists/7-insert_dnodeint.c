c80c21bc49b9ce724d4e39451721253fa873e80a
Patr1k00 <shalsipatrik00@gmail.com>
2023-04-06 01:17:05 -0700

    al

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,35 +1,43 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-
+#include <stddef.h>
+#include <stdlib.h>
 /**
- * insert_dnodeint_at_index - function that inserts a new node at a given
- * position
- * @h: head
- * @idx: index of node
- * @n: element of node
- * Return: the address of the new node, or NULL if it failed
+ * insert_dnodeint_at_index - function that inserts a node at a given pos
+ * @h: list head
+ * @idx: where to insert node
+ * @n: value
+ * Return: ptr to node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t));
-	dlistint_t *tmp = *h;
+	dlistint_t *new, *temp;
+	unsigned int count = 0;
 
+	temp = *h;
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+	while (count != idx - 1)
+	{
+		if (temp == NULL)
+			return (NULL);
+		temp = temp->next;
+		count++;
+	}
+	if (temp->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
-	while (idx != 1)
-	{
-		tmp = tmp->next;
-		idx--;
-	}
 	new->n = n;
-	new->next = tmp->next;
-	new->prev = tmp;
-	if (tmp->next != NULL)
-		tmp->next->prev = new;
-	tmp->next = new;
-
+	new->next = temp->next;
+	new->prev = temp;
+	temp->next->prev = new;
+	temp->next = new;
 	return (new);
+
 }
